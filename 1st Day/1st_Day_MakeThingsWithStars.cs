c8f92be9443cc6e84e4f05af165da663549c2f66
Jean-Gyu Hu <gjwlsrb1700@gmail.com>
2018-03-31 12:57:34 +0900

    EN#_Study_1st_day_2018_03_30_

---
+++ 1st Day/1st_Day_MakeThingsWithStars.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENjoy_day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("피라미드");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size - i; j++)
+                    Console.Write(" ");
+                for(int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("역피라미드");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = size-1; i >= 0; i--){
+
+                for (int j = 0; j < size - i; j++)
+                    Console.Write(" ");
+                for(int j = 2 * i+1; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("모래시계");
+
+            size = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = size - 1; i > 0; i--)
+            {
+
+                for (int j = 0; j < size - i; j++)
+                    Console.Write(" ");
+                for (int j = 2 * i + 1; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("다이아");
+
+            size = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < size-1; i++)
+            {
+                for (int j = 0; j < size - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+            for (int i = size - 1; i >= 0; i--)
+            {
+
+                for (int j = 0; j < size - i; j++)
+                    Console.Write(" ");
+                for (int j = 2 * i + 1; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
