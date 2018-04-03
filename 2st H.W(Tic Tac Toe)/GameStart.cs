007fd7b5a1f73a659ff2c0f87adeba892cbd595d
Jean-Gyu Hu <gjwlsrb1700@gmail.com>
2018-04-03 10:40:56 +0900

    2st H.W (Tic Tac Toe Game)

---
+++ 2st H.W(Tic Tac Toe)/GameStart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enjoy_Day2
+{
+    class GameStart
+    {
+        static void Main(string[] args)
+        {
+            StartMenu startMenu = new StartMenu();
+        }
+    }
+}
---
+++ 2st H.W(Tic Tac Toe)/MakeUserID.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enjoy_Day2
+{
+    class MakeUserID
+    {
+        private String strPlayerOne,strPlayerTwo;
+        private VsUserMode vsUserMode;
+        private VsComputerMode vsComputerMode;
+
+        public MakeUserID(int mode)
+        {
+            switch (mode)
+            {
+                case 1:
+                    Console.Write("\n\n\t\t첫번째 사용자의 이름을 입력하세요 : ");
+                    strPlayerOne = Console.ReadLine();
+                    Console.Write("\n\n\t\t두번째 사용자의 이름을 입력하세요 : ");
+                    strPlayerTwo = Console.ReadLine();
+                    Console.Clear();
+                    vsUserMode = new VsUserMode(strPlayerOne, strPlayerTwo);
+                    break;
+
+                case 2:
+                    Console.Write("\n\n\t\t컴퓨터와 대결할 사용자의 이름을 입력하세요 : ");
+                    strPlayerOne = Console.ReadLine();
+                    vsComputerMode = new VsComputerMode(strPlayerOne);
+                    break;
+
+                default:
+                    
+                    break;
+            }
+            
+
+
+        }
+    }
+}
