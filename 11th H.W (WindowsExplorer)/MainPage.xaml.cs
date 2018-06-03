2dd08ef101e6c402e88dcfd5568a586250d46ac2
Jean-Gyu Hu <gjwlsrb1700@gmail.com>
2018-06-03 18:33:01 +0900

    feat: 사이드 바에 기능 추가 & 메인창에도 파일 표시되게 함

---
+++ 11th H.W (WindowsExplorer)/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hu_s_WindowExplorer
+{
+    /// <summary>
+    /// MainPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SetMainPage();
+        }
+
+        public void SetMainPage()
+        {
+            listView.Items.Clear();    // 전에 있던 목록들을 지우고 새로 나열함.
+
+            string[] directories = Directory.GetDirectories("C:\\Users\\gjwls\\Desktop");
+
+            foreach (string directory in directories)    // 폴더 나열
+            {
+                DirectoryInfo info = new DirectoryInfo(directory);
+                Image img = new Image();
+                img.Source = new BitmapImage(new Uri("Images\\folder.png",UriKind.Relative));
+                StackPanel stack = new StackPanel();
+               
+                Label lbl = new Label();
+                lbl.Width = 40;
+                lbl.Content = info.Name;
+                img.Width = 30;
+                img.Height = 30;
+                stack.Children.Add(img);
+                stack.Children.Add(lbl);
+                listView.Items.Add(stack);
+            }
+
+            string[] files = Directory.GetFiles("C:\\Users\\gjwls\\Desktop");
+
+            foreach (string file in files)    // 파일 나열
+            {
+                FileInfo info = new FileInfo(file);
+                StackPanel stack = new StackPanel();
+                Image img = new Image();
+                Label lbl = new Label();
+                lbl.Content = info.Name;
+                lbl.Width = 40;
+                
+                img.Source = GetIcon(file);
+                img.Width = 30;
+                img.Height = 30;
+                stack.Children.Add(img);
+                stack.Children.Add(lbl);
+                listView.Items.Add(stack);
+            }
+        }
+
+        public ImageSource GetIcon(string filename)       //File 경로에서 Image 추출
+        {
+            ImageSource icon;
+
+            using (System.Drawing.Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(filename))
+            {
+                icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                sysicon.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            }
+            return icon;
+        }
+    } 
+}
---
+++ 11th H.W (WindowsExplorer)/SideBar.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace Hu_s_WindowExplorer
+{
+    /// <summary>
+    /// SideBar.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SideBar : UserControl
+    {
+        string[] drives;
+        TreeViewItem[] treeRoots;
+
+        public SideBar()
+        {
+            InitializeComponent();
+            
+            drives = GetDrives();
+            MakeTreeView();
+        }
+        
+        public string[] GetDrives()
+        {
+            return Environment.GetLogicalDrives();
+        }
+
+        public void SetTreeChild(string drive, TreeViewItem root)
+        {
+            string path = drive;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            foreach(var files in directoryInfo.GetDirectories().Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden)))
+            {
+                try
+                {
+                    TreeViewItem item = new TreeViewItem() { Header = files.Name };
+                    root.Items.Add(item);
+                    SetTreeChild(files.FullName.ToString(), item);
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
+        }
+
+        public void MakeTreeView()
+        {
+            int count = 0;
+            treeRoots = new TreeViewItem[drives.Length];
+
+            for(int i = 0; i < drives.Length; i++)
+            {
+                treeRoots[i] = new TreeViewItem() { Header = drives[i] };
+            }
+
+            foreach(TreeViewItem item in treeRoots)
+            {
+                mainTreeView.Items.Add(item);
+                SetTreeChild(drives[count],item);
+                count++;
+            }
+        }
+    }
+}
+
+
+
+
+
+//DirectorySecurity dSecurity = Directory.GetAccessControl(files.ToString());
+//dSecurity.AddAccessRule(new FileSystemAccessRule(
+//       new NTAccount("Users"),
+//      FileSystemRights.FullControl,
+//       InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit,
+//       PropagationFlags.None,
+//       AccessControlType.Allow
+//));
+//   Directory.SetAccessControl(files.ToString(), dSecurity);
